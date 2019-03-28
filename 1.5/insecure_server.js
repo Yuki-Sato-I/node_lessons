d1aba3e6ef5264e8f88230e80cc05c527a95971e
YukiSato <yukiyukiamam@gmail.com>
2019-03-28 22:33:44 +0900

    第１章終了

--- 1.5/insecure_server.js
+++ 1.5/insecure_server.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 
 http.createServer(function (request, response) {
   var lookup = url.parse(decodeURI(request.url)).pathname;
+  lookup = path.normalize(lookup);
   lookup = (lookup === '/') ? '/index.html' : lookup;
   var f = 'content' + lookup;
   console.log(f);
--- 1.5/pseudosafe_server.js
+++ 1.5/pseudosafe_server.js
@@ -5,20 +5,19 @@ var fs = require('fs');//file status
 
 http.createServer(function (request, response) {
   var lookup = url.parse(decodeURI(request.url)).pathname;
-  lookup = (lookup === '/') ? '/index-serve.html' : lookup + '-serve';
+  lookup = (lookup === '/') ? '/index.html-serve' : lookup + '-serve';
   var f = 'content-pseudosafe' + lookup;
   console.log(f);
-
   fs.exists(f, function (exists) {
     if (!exists) {
       response.writeHead(404);
       response.end();
       return;
     }
+    fs.readFile(f, function (err, data) {
+      response.end(data);
+    });
   });
 
-  fs.readFile(f, function (err, data) {
-    response.end(data);
-  });
 
 }).listen(8080);
